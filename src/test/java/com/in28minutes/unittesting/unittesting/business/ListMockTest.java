8c9903b502fa2a12763f339b86211df34dfd4c2a
Peter <pgkutc@gmail.com>
2020-12-28 14:02:37 -0500

    more arraylist mocking and the concept of spying

--- src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ListMockTest.java
@@ -1,16 +1,27 @@
 package com.in28minutes.unittesting.unittesting.business;
 
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.atMost;
 
 public class ListMockTest {
 
-    List mock = mock(List.class);
+    List<String> mock = mock(List.class);
 
     @Test
     public void sizeBasicTest() {
@@ -32,4 +43,71 @@ public class ListMockTest {
         Assertions.assertEquals(null, mock.get(1));
     }
 
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("in28minutes");
+        Assertions.assertEquals("in28minutes", mock.get(0));
+        Assertions.assertEquals("in28minutes", mock.get(1));
+    }
+
+    @Test
+    public void verificationBasics() {
+        String value1 = mock.get(0);
+        String value2 = mock.get(1);
+        verify(mock).get(0);
+        verify(mock, times(2)).get(anyInt());
+        verify(mock, atLeast(1)).get(anyInt());
+        verify(mock, atLeastOnce()).get(anyInt());
+        verify(mock, atMost(2)).get(anyInt());
+        verify(mock, never()).get(2);
+    }
+
+    @Test
+    public void argumentCapturing() {
+        mock.add("SomeString");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock).add(captor.capture());
+        Assertions.assertEquals("SomeString", captor.getValue());
+    }
+
+    @Test
+    public void MultipleArgumentCapturing() {
+        mock.add("SomeString1");
+        mock.add("SomeString2");
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(mock, times(2)).add(captor.capture());
+        Assertions.assertEquals("SomeString1", captor.getAllValues().get(0));
+        Assertions.assertEquals("SomeString2", captor.getAllValues().get(1));
+    }
+
+    @Test
+    public void mocking() {
+        ArrayList arrayListMock = mock(ArrayList.class);
+        System.out.println(arrayListMock.get(0));
+        System.out.println(arrayListMock.size());
+        arrayListMock.add("Test");
+        arrayListMock.add("Test2");
+        System.out.println(arrayListMock.size());
+        when(arrayListMock.size()).thenReturn(5);
+        System.out.println(arrayListMock.size());
+    }
+
+    @Test
+    public void spying() {
+        ArrayList arrayListSpy = spy(ArrayList.class);
+        arrayListSpy.add("Test0");
+        System.out.println(arrayListSpy.get(0));//Test0
+        System.out.println(arrayListSpy.size());//1
+        arrayListSpy.add("Test");
+        arrayListSpy.add("Test2");
+        System.out.println(arrayListSpy.size());//3
+
+        when(arrayListSpy.size()).thenReturn(5);
+        System.out.println(arrayListSpy.size());//5
+
+        arrayListSpy.add("Test4");
+        System.out.println(arrayListSpy.size());//5
+
+        verify(arrayListSpy).add("Test4");
+    }
 }
