36b487a3cdbc8dd9d6a9ae41569839030354600b
Peter <pgkutc@gmail.com>
2020-12-29 08:51:15 -0500

    create a service test

---
+++ src/test/java/com/in28minutes/unittesting/unittesting/business/ItemBusinessServiceTest.java
@@ -0,0 +1,35 @@
+package com.in28minutes.unittesting.unittesting.business;
+
+import com.in28minutes.unittesting.unittesting.data.ItemRepository;
+import com.in28minutes.unittesting.unittesting.model.Item;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemBusinessServiceTest {
+
+    @InjectMocks
+    private ItemBusinessService itemBusinessService;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @Test
+    public void calculateSumUsingDataServiceBasic() {
+        when(itemRepository.findAll()).thenReturn(Arrays.asList(new Item(2, "Item2", 10, 10),
+                new Item(3, "Item3", 20, 20)));
+        List<Item> items = itemBusinessService.retrieveAllItems();
+        Assertions.assertEquals(100, items.get(0).getValue());
+        Assertions.assertEquals(400, items.get(1).getValue());
+    }
+
+}
--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
@@ -2,7 +2,6 @@ package com.in28minutes.unittesting.unittesting.controller;
 
 import com.in28minutes.unittesting.unittesting.business.ItemBusinessService;
 import com.in28minutes.unittesting.unittesting.model.Item;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,6 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -35,7 +36,7 @@ public class ItemControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}"))
                 .andReturn();
-        Assertions.assertEquals("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}", result.getResponse().getContentAsString());
+//        Assertions.assertEquals("{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}", result.getResponse().getContentAsString());
     }
 
     @Test
@@ -50,6 +51,23 @@ public class ItemControllerTest {
         MvcResult result = mockMvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(content().json("{id:2,name:Item2,price:10}"))
+                .andReturn();
+//        Assertions.assertEquals("{id:2,name:Item2,price:10,quantity:10}", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void retrieveAllItemsBasicTest() throws Exception {
+        when(businessService.retrieveAllItems()).thenReturn(
+                Arrays.asList(new Item(2, "Item2", 10, 10),
+                        new Item(3, "Item3", 20, 20)));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/all-items-from-database")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{id:2,name:Item2,price:10},{id:3,name:Item3,price:20}]"))
                 .andReturn();
 //        Assertions.assertEquals("{id:2,name:Item2,price:10,quantity:10}", result.getResponse().getContentAsString());
     }
