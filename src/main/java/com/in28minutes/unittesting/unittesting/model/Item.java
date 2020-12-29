11f6496bb01f61e82d9d3cbb45b1b8256041c17b
Peter <pgkutc@gmail.com>
2020-12-28 19:07:52 -0500

    created a RESTful service and added pretty json to application properties

--- src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
+++ src/main/java/com/in28minutes/unittesting/unittesting/model/Item.java
@@ -1,10 +1,23 @@
 package com.in28minutes.unittesting.unittesting.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class Item {
+
+    @Id
     private int id;
     private String name;
     private int price;
     private int quantity;
+
+    @Transient
+    private int value;
+
+    public Item() {
+    }
 
     public Item(int id, String name, int price, int quantity) {
         this.id = id;
@@ -13,20 +26,44 @@ public class Item {
         this.quantity = quantity;
     }
 
-    public int getPrice() {
-        return price;
+    public int getId() {
+        return id;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getId() {
-        return id;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     @Override
