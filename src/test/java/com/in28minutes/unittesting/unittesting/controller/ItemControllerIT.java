40eb67b6cb14982b7e5cbac83bf148be5f0b2b26
Peter <pgkutc@gmail.com>
2020-12-29 10:26:24 -0500

    added some functional programming

--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerIT.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ItemControllerIT {
 
     @Autowired
